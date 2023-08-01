175643496bbe93120e862f4c19571aed54cb7828
Łukasz Wróblewski <->
2023-08-01 18:01:52 +0200

    ValidEmail

--- validators.go
+++ validators.go
@@ -5,3 +5,7 @@ import "regexp"
 var (
 	emailRegex = regexp.MustCompile(`^([a-zA-Z0-9.!#$%&*+/=?^_{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*)?$`)
 )
+
+func ValidEmail(s string) bool {
+	return emailRegex.MatchString(s)
+}
