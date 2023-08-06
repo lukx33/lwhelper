e74bf0d73f88295ad24a66b7b902e85f52545283
Łukasz Wróblewski <->
2023-08-06 08:28:20 +0200

    Mandatory

--- env.go
+++ env.go
@@ -2,17 +2,21 @@ package lwhelper
 
 import "syscall"
 
-func GetEnv(key, def string, mandatory bool) func() string {
+func GetEnv(key, def string) func() string {
 
 	tmp, _ := syscall.Getenv(key)
 	if tmp == "" {
-		if def == "" && mandatory {
-			panic("config `" + key + "` is empty")
-		}
 		tmp = def
 	}
 
 	return func() string {
 		return tmp
 	}
+}
+
+func Mandatory(s string) string {
+	if s == "" {
+		panic("config is empty")
+	}
+	return s
 }
