6891ac7339940e7caae1da82cc92ae4816543abf
Łukasz Wróblewski <->
2023-11-10 18:12:49 +0100

    OsArg

--- os.go
+++ os.go
@@ -5,3 +5,10 @@ import "os"
 func ReadFile(filepath string) string {
 	return string(Must(os.ReadFile(filepath)))
 }
+
+func OsArg(idx int) string {
+	if len(os.Args) <= idx {
+		return append(os.Args, make([]string, idx)...)[idx]
+	}
+	return os.Args[idx]
+}
--- strings.go
+++ strings.go
@@ -41,3 +41,8 @@ func ToInt(s string) int {
 	i, _ := strconv.Atoi(strings.TrimSpace(s))
 	return i
 }
+
+func ToInt64(s string) int64 {
+	i, _ := strconv.ParseInt(strings.TrimSpace(s), 10, 64)
+	return i
+}
